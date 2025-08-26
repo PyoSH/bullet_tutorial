3dcd66c94dfc33a9b109e879a3de2aea0695f51c
PyoSH <jeongmok99@koreatech.ac.kr>
2025-08-26 18:14:57 +0900

    testing with ER lecture in MACRO!

--- hello_bullet.py
+++ hello_bullet.py
@@ -4,8 +4,14 @@ import pybullet_data
 import os
 
 p.connect(p.GUI)
-# pandaUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(),'franka_panda/panda.urdf'), useFixedBase=True)
-pandaUid = p.loadURDF(os.path.join('models','testing1.urdf'), useFixedBase=True)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
 
-while True:
+# pandaUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(),'franka_panda/panda.urdf'), useFixedBase=True)
+pandaUid = p.loadURDF(os.path.join('models','testing1_macro.urdf'), useFixedBase=True)
+
+for i in range(10000):
     p.stepSimulation()
+    time.sleep(1./240.)
+
+p.disconnect()
